0b26e83ce7ad2b6512639f75c5de589e1cb8c80c
Zahraa <alwarda@hotmail.co.uk>
2021-09-05 13:37:47 -0700

    display single movie page elements

--- src/components/Movies/SignleMovie.jsx
+++ src/components/Movies/SignleMovie.jsx
@@ -1,27 +1,57 @@
 import React from 'react';
-// import PropTypes from 'prop-types';
+import { useParams } from 'react-router';
+import { useSelector } from 'react-redux';
+import useMoviesFetch from '../hooks/useMoviesFetch';
 
+const imgPath = `https://www.themoviedb.org/t/p/w440_and_h660_face`
+const  SingleMoviePage = () => {
+    //  Movie Id
+     const {movieId} = useParams()
+    //  getting the list of filtered movies from the store
+     const filteredMovies = useSelector(state => state.moviesFilter.movies.results)
 
-const  SingleMoviePage = () => <h1>Single Movie Page</h1>
-//  {
-//   const {title, original_language, overview, poster_path, release_date} = movie
-//   return(
-//     <>
-//   <p>{title}</p>
-//   <p>{original_language}</p>
-//   <p>{overview}</p>
-//   <p>{poster_path}</p>
-//   <p>{release_date}</p>
-//   </>
-//   )
+     // mapping over filtered movies and find the movie that its id === to the pressed id
+     const movie = filteredMovies?.find(_movie => _movie.id === +movieId)
+    //  destructure movie items
+    //  fetching actors and similar movies that belong to this movie
+    const {data: actors, isLoaded} = useMoviesFetch(`https://api.themoviedb.org/3/movie/${movie?.id}/credits?api_key=9c1bd422dc4c265552caa0ce7241a530&language=en-US/cast`)
+    const {data: similarMovies} = useMoviesFetch(`https://api.themoviedb.org/3/movie/${movie?.id}/similar?api_key=9c1bd422dc4c265552caa0ce7241a530&language=en-US&page=1`)
+    
+    if(!isLoaded) return <p>Loading ...</p>
+    const director = actors?.crew?.find(_crew => _crew.job === "Director")
+    const {title, original_language, overview, poster_path, release_date, vote_count} = movie
+  return(
+    <>
+  {!isLoaded? 
+   <p>Loading ...</p>:
+   <>
+  <p>{title}</p>
+  <p>{original_language}</p>
+  <p>{overview}</p>
+  <img  src={`${imgPath}${poster_path}`} alt={title}/>
+  <p>{release_date}</p>
+  <p>Votes: {vote_count}</p>
+  <hr />
+
+  {/* Actors List */}
+  <ul><h1>Actors: </h1>
+      {actors.cast.slice(0, 5).map(actor => <li key={actor.id}>{actor.name}<img src={`${imgPath}${actor.profile_path}`} alt={actor.name}/></li>)}
+  </ul>
+  <hr />
+
+ {/* Director Name */}
+ <h1>Director:{director.name} </h1> 
+  <img src={`${imgPath}${director.profile_path}`}  alt={director.name}/>
+
+<br/>
+ {/* Related Movies */}
+<h1>Related Movies</h1>
+{similarMovies?.results?.slice(0, 5).map(_similarMovie => <li key={_similarMovie.id}>{_similarMovie.title}<img src={`${imgPath}${_similarMovie.poster_path}`} alt={_similarMovie.title}/></li>)}
+ </> }
+  </>
+  )
   
-// }
+}
 
-// SingleMoviePage.propTypes = {
-//   movie: PropTypes.shape({
-//     title: PropTypes.string, original_language: PropTypes.string,
-//  overview: PropTypes.string, poster_path: PropTypes.string, release_date: PropTypes.string,
-//   }).isRequired,
-// }
 
 export default SingleMoviePage
