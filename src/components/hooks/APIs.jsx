6213b335ace7a62f8c0faddfcbe7fd5afe07b899
Zahraa <alwarda@hotmail.co.uk>
2021-09-05 13:35:44 -0700

    fetch upcoming, top rated, and now playing api

--- src/components/hooks/APIs.jsx
+++ src/components/hooks/APIs.jsx
@@ -3,5 +3,8 @@ export const  API_KEY = `9c1bd422dc4c265552caa0ce7241a530`;
 export const TRENDING_MOVIES= `https://api.themoviedb.org/3/trending/all/day?api_key=${API_KEY}`
 export const POPULAR_MOVIES= `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=en-US&page=2`
 export const GENRES= `https://api.themoviedb.org/3/genre/movie/list?api_key=${API_KEY}&language=en-US`
+export const TOP_RATED= `https://api.themoviedb.org/3/movie/top_rated?api_key=${API_KEY}&language=en-US&page=1`
+export const NOW_PLAYING= `https://api.themoviedb.org/3/movie/now_playing?api_key=${API_KEY}&language=en-US&page=1`
+export const UPCOMING= `https://api.themoviedb.org/3/movie/upcoming?api_key=${API_KEY}&language=en-US&page=1`
 
    
