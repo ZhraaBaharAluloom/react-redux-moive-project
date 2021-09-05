e35b9568eb8c749dd27b5bd54e5d96d37fd7b97a
Zahraa <alwarda@hotmail.co.uk>
2021-09-05 13:36:33 -0700

    save filtered movie lost in the store

--- src/components/Movies/index.jsx
+++ src/components/Movies/index.jsx
@@ -1,17 +1,21 @@
-import React, { useContext } from 'react';
+import React from 'react';
+import { useDispatch ,useSelector} from 'react-redux';
+import { fetchFilteredMovies } from '../../storage/reducer';
 
 // Hooks
 import { API_KEY } from '../hooks/APIs';
-import { GenreContext } from '../hooks/useContex';
 import useMoviesFetch from '../hooks/useMoviesFetch';
 
 // Components
 import MovieItem from './MovieItem';
 
 const MovieList = () => {
-  const [genre] = useContext(GenreContext)   
+  const genre = useSelector(state => state.moviesFilter.genre)
+   const dispatch = useDispatch()
   const {data: filteredMovies, isLoaded} = useMoviesFetch(`https://api.themoviedb.org/3/discover/movie?with_genres=${genre}&api_key=${API_KEY}&language=en-US&page=1&with_watch_monetization_types=flatrate`)
   const filteredMovieList =  filteredMovies?.results?.map(movie => <MovieItem movie={movie} key={movie.id}/> )
+
+  dispatch(fetchFilteredMovies(filteredMovies))
 
   if(!isLoaded) return <p>Loading ...</p>
     return(
