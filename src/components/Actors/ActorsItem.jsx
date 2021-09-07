5db0e56570691bc3487f64985112973ba692038a
Zahraa <alwarda@hotmail.co.uk>
2021-09-06 19:03:45 -0700

    create actors component

--- src/components/Actors/ActorsItem.jsx
+++ src/components/Actors/ActorsItem.jsx
@@ -1,11 +1,29 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { Card } from 'react-bootstrap';
+import "./Styles.css"
 
-const ActorsItem = () => {
+
+const ActorsItem = ({actor}) => {
+    const {name, profile_path} = actor
     return (
-        <div>
-            
-        </div>
+    <Card  className="actor-card">
+    <Card.Img variant="top"src={`	https://www.themoviedb.org/t/p/w470_and_h470_face${profile_path}`} alt={name} />
+    <Card.Body>
+        <Card.Title className="card-title">Name: {name}</Card.Title>
+    </Card.Body>
+    </Card>
+       
     );
 };
+
+ActorsItem.propTypes = {
+    actor: PropTypes.shape({
+      name: PropTypes.string,
+      profile_path: PropTypes.number
+      
+    }).isRequired,
+  }
+  
 
 export default ActorsItem;
--- src/components/Actors/index.jsx
+++ src/components/Actors/index.jsx
@@ -1,6 +1,34 @@
 import React from 'react';
 
-const  Actors = () => <h1>Actors Page</h1>
+// Store
+import { useDispatch } from 'react-redux';
+import { fetchActors } from '../../storage/reducer';
+
+// Hooks
+import { ACTORS } from '../hooks/APIs';
+import useMoviesFetch from "../hooks/useMoviesFetch"
+
+// Components
+import ActorsItem from "./ActorsItem"
+
+const  Actors = () => {
+ const dispatch = useDispatch()
+
+const {data: actors, isLoaded} = useMoviesFetch(ACTORS)
+
+dispatch(fetchActors(actors))
+
+if(!isLoaded) return <p>Loading</p>
+return(
+    <>
+        <h1>Actors</h1>
+        <div style={{display: "flex", flexWrap: "wrap", justifyContent: "center"}}>
+            {actors.results.map(actor => <ActorsItem actor={actor}  key={actor.id}/>)}
+        </div>
+    </>
+)
+
+}
 
 
 export default Actors
